bd5cf9e940d85a2786b68ab1ed1d2bb279467d97
Tushar-Keshari <96391653+Tushar-Keshari@users.noreply.github.com>
2023-01-04 14:48:57 +0530

    Add files via upload

---
+++ bullandcows.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#include <iostream>
+typedef long long ll;
+using namespace std;
+int main(){
+    string str="1089";
+    str[3] = '\0';
+    cout<<str<<endl;
+    return 0;
+}
